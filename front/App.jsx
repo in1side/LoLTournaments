4e1d6fe6933c633aaa35cb97e84b8335e9ff1fd0
Branden Soropia <b.soropia@gmail.com>
2017-01-03 20:05:16 -0500

    Can login and logout, but doesn't redirect back to home on logout.

--- front/App.jsx
+++ front/App.jsx
@@ -1,19 +1,76 @@
 'use strict'
 
 import React, { Component } from 'react'
+import Auth0Lock from 'auth0-lock'
 
 // Views
 import Home from './Home'
 
 // material-ui
 import AppBar from 'material-ui/AppBar'
+import FlatButton from 'material-ui/FlatButton'
+
+const CLIENT_ID = 'IqpajO4vcyVQPeEKHtMS9J6K0Ky1MQHl'
+const DOMAIN = 'bsoropia.auth0.com'
 
 export default class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      isLoggedIn: false
+    }
+
+    // Auth0 lock config and initialization
+    this.lock = new Auth0Lock(CLIENT_ID, DOMAIN, {
+      auth: {
+        redirectUrl: 'http://localhost:8080',
+        responseType: 'token'
+      },
+      additionalSignUpFields: [{
+        name: 'Username',
+        placeholder: 'Username'
+      }],
+      loginAfterSignUp: true,
+      rememberLastLogin: true
+    })
+
+    this.lock.on('authenticated', (authResult) => {
+      // Use the token in authResult to getProfile() and save it to localStorage
+      this.lock.getProfile(authResult.idToken, (error, profile) => {
+        if (error) {
+          console.log(error)
+          return
+        }
+
+        localStorage.setItem('idToken', authResult.idToken)
+        localStorage.setItem('profile', JSON.stringify(profile))
+        this.setState({ isLoggedIn: true })
+        console.log(profile)
+      })
+    })
+  }
+
+  toggleLoginLogoutButton = () => {
+    return this.state.isLoggedIn
+      ? <FlatButton label='Logout' onTouchTap={() => {
+        this.setState({ isLoggedIn: false })
+        localStorage.removeItem('idToken')
+        localStorage.removeItem('profile')
+        // TODO: Redirect back to home
+        this.lock.logout()
+      }} />
+      : <FlatButton label='Login' onTouchTap={() => {
+        this.lock.show()
+      }} />
+  }
+
   render () {
     return (
       <div className='App'>
         <AppBar
           title='LoL Tournaments'
+          showMenuIconButton={false}
+          iconElementRight={this.toggleLoginLogoutButton()}
         />
         <Home />
       </div>
